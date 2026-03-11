139cbf8234878392e5b00d1ac613715f3a46814b
SteniusJ <stenius.patrik@gmail.com>
2026-03-11 11:31:07 +0200

    app, hyperparameters

---
+++ src/app.py
@@ -0,0 +1,48 @@
+import cv2
+from ultralytics import YOLO
+import numpy as np
+import math
+
+model = YOLO('./runs/output/train/weights/best.pt')
+names = model.names
+
+video_path = 0
+
+video_capture = cv2.VideoCapture(video_path)
+
+if not video_capture.isOpened():
+    print('Error opening video stream')
+    exit
+
+while video_capture.isOpened():
+    status, frame = video_capture.read()
+
+    if status:
+        results = model(frame)
+
+        for result in results:
+            boxes = result.boxes.xyxy.numpy()
+            class_names = []
+
+            for c in result.boxes.cls:
+                class_names.append(names[int(c)])
+            
+            print('class names:', class_names)
+
+            for index, box in enumerate(boxes):    
+                rwidth = box/frame.shape[1]
+
+                #                     video   cordinater för rectangle                               färg brg   pixel bredd
+                frame = cv2.rectangle(frame, (int(box[0]), int(box[1])), (int(box[2]), int(box[3])), (255, 0, 0), 20)    
+                frame = cv2.putText(frame, class_names[index], (int(box[0]), int(box[1]) - 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
+
+        frame = cv2.resize(frame, None, fx=1.2, fy=1.2, interpolation=cv2.INTER_NEAREST)
+        cv2.imshow("Avfallspolisen", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
--- src/train.py
+++ src/train.py
@@ -4,5 +4,29 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')
 
 results = model.train(data='../datasets/mydataset.yml',
-                      epochs=200,
+                      epochs=10000,
+                      patience=15,
+                      flipud=0.2,
+                      mixup=0.2,
+                      copy_paste=0.1,
+                      degrees=35.0,
+                      shear=5.0,
+                      hsv_v=0.6,
+                      hsv_h=0.1,  
+                      dropout=0.2,
+                      cutmix=0.1,
+                      label_smoothing=0.1,
                       project='../output')
+
+# --- Augmentering ---
+
+#flipud=0.2, # Speglar bilden vertikalt med 20% sannolikhet – ökar variation
+#mixup=0.2, # Blandar två bilder och deras etiketter – hjälper generalisering
+#copy_paste=0.1, # Klistrar in objekt från andra bilder – skapar fler träningsexempel
+#degrees=15.0, # Roterar bilden slumpmässigt upp till ±15 grader
+#shear=5.0, # Förvränger bilden lätt – simulerar olika kameravinklar
+
+# --- Regularisering ---
+
+#dropout=0.2, # Stänger av 20% av neuronen slumpmässigt under träning – motverkar overfitting
+#label_smoothing=0.1 # Mjukar upp etiketterna (t.ex. 1.0 → 0.9) – minskar överkonfidens)
